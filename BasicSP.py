06193034310163d19e3e1bac80975645ea1060d2
Sudhanshu Apte <sudhanshu.apte@tu-ilmenau.de>
2022-07-17 13:21:31 +0200

    Added noise in the data

--- BasicSP.py
+++ BasicSP.py
@@ -6,19 +6,23 @@ f_s = 1000   # Sampling frequency
 f_sig = 100  # Signal frequency
 n_fft_pts = 64
 t = np.linspace(0, n_fft_pts/f_s, n_fft_pts)
+noise = np.random.normal(0,1,n_fft_pts)
 
 # Generate some signal
 
-sine_wave = np.sin(2*np.pi*f_sig*t)
+sine = np.sin(2*np.pi*f_sig*t) + noise
+cosine = np.cos(2*np.pi*f_sig*t) + noise
+
+complex_signal = cosine+1j*sine
 
 # Frequency domain transformation
 
-fft_out = np.fft.fftshift(np.abs(np.fft.fft(sine_wave)))
+fft_out = np.fft.fftshift(np.abs(np.fft.fft(complex_signal)))
 
 # Plot the original signal and results
 
 plt.figure(1)
-plt.plot(sine_wave)
+plt.plot(sine)
 plt.title('Original Signal')
 plt.xlabel('Samples')
 plt.ylabel('Amplitude')
